6dc2b293110703254cf50e2ff8f3062d8938a987
ayakadai <ayakadai615@gmail.com>
2026-08-01 14:47:24 +0900

    test: add test database setup

---
+++ tests/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+TEST_DATABASE_URL = "sqlite:///./test.db"
+
+# テスト用のエンジンを作成
+testing_engine = create_engine(
+    TEST_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+# テスト用セッションを生成するクラス
+TestingSessionLocal = sessionmaker(
+    bind=test_engine,
+    autocommit=False,
+    autoflush=False,
+)
+
+
+# 開発用DBへの依存を、テスト用DBへ差し替える
+def override_get_db():
+    db = TestingSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
